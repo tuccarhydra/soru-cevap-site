04e26a5a5173a0744b08b252b927ae0c86994987
tuccarhydra <61882037+tuccarhydra@users.noreply.github.com>
2020-03-06 19:30:39 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,346 @@
+      function soru1(){   //  <---------- 1.soru   ---------->
+     if("hiding" == status) {
+                alert("zaten gizli durumda");
+            } else {
+				var javascriptDivObject=document.getElementById("s1");
+                javascriptDivObject.style.display="none";
+                status = "hiding";
+
+				
+				var javascriptDivObject=document.getElementById("mesaj2");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+				var javascriptDivObject=document.getElementById("cevab2v1");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+				var javascriptDivObject=document.getElementById("cevab2v2");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+				var javascriptDivObject=document.getElementById("cevab2v3");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+				var javascriptDivObject=document.getElementById("cevab2v4");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+            }
+		    var sonuc=document.getElementById("dogru");
+			sonuc.value=Number(sonuc.value)+1;
+			var sonuc=document.getElementById("toplam");
+			sonuc.value=Number(sonuc.value)+1;
+}
+      function soruv1(){
+	  if("hiding" == status) {
+                alert("zaten gizli durumda");
+            } else {
+				var javascriptDivObject=document.getElementById("s1");
+                javascriptDivObject.style.display="none";
+                status = "hiding";
+
+				
+				var javascriptDivObject=document.getElementById("mesaj2");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+				var javascriptDivObject=document.getElementById("cevab2v1");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+				var javascriptDivObject=document.getElementById("cevab2v2");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+				var javascriptDivObject=document.getElementById("cevab2v3");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+				var javascriptDivObject=document.getElementById("cevab2v4");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+            }
+		    var sonuc=document.getElementById("yanlis");
+			sonuc.value=Number(sonuc.value)+1;
+			var sonuc=document.getElementById("toplam");
+			sonuc.value=Number(sonuc.value)+1;
+}	
+//  <---------- 1.soru sonu  ---------->
+      function soru2(){  //  <---------- 2.soru   ---------->
+     if("hiding" == status) {
+                alert("zaten gizli durumda");
+            } else {
+				var javascriptDivObject=document.getElementById("s2");
+                javascriptDivObject.style.display="none";
+                status = "hiding";
+
+				
+				
+				var javascriptDivObject=document.getElementById("mesaj3");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+				var javascriptDivObject=document.getElementById("cevab3v1");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+				var javascriptDivObject=document.getElementById("cevab3v2");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+				var javascriptDivObject=document.getElementById("cevab3v3");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+				var javascriptDivObject=document.getElementById("cevab3v4");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+            }
+		    var sonuc=document.getElementById("dogru");
+			sonuc.value=Number(sonuc.value)+1;
+			var sonuc=document.getElementById("toplam");
+			sonuc.value=Number(sonuc.value)+1;
+}
+      function soruv2(){
+	  if("hiding" == status) {
+                alert("zaten gizli durumda");
+            } else {
+				var javascriptDivObject=document.getElementById("s2");
+                javascriptDivObject.style.display="none";
+                status = "hiding";
+
+				var javascriptDivObject=document.getElementById("mesaj3");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+				var javascriptDivObject=document.getElementById("cevab3v1");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+				var javascriptDivObject=document.getElementById("cevab3v2");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+				var javascriptDivObject=document.getElementById("cevab3v3");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+				var javascriptDivObject=document.getElementById("cevab3v4");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+            }
+		    var sonuc=document.getElementById("yanlis");
+			sonuc.value=Number(sonuc.value)+1;
+			var sonuc=document.getElementById("toplam");
+			sonuc.value=Number(sonuc.value)+1;
+}	
+//  <---------- 2.soru sonu  ---------->
+		function soru3(){  //  <---------- 3.soru   ---------->
+		if("hiding" == status) {
+                alert("zaten gizli durumda");
+            } else {
+				var javascriptDivObject=document.getElementById("s3");
+                javascriptDivObject.style.display="none";
+                status = "hiding";
+
+				
+				
+				var javascriptDivObject=document.getElementById("mesaj4");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+				var javascriptDivObject=document.getElementById("cevab4v1");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+				var javascriptDivObject=document.getElementById("cevab4v2");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+				var javascriptDivObject=document.getElementById("cevab4v3");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+				var javascriptDivObject=document.getElementById("cevab4v4");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+            }
+		    var sonuc=document.getElementById("dogru");
+			sonuc.value=Number(sonuc.value)+1;
+			var sonuc=document.getElementById("toplam");
+			sonuc.value=Number(sonuc.value)+1;
+}
+      function soruv3(){
+	  if("hiding" == status) {
+                alert("zaten gizli durumda");
+            } else {
+				var javascriptDivObject=document.getElementById("s3");
+                javascriptDivObject.style.display="none";
+                status = "hiding";
+
+				
+				
+				var javascriptDivObject=document.getElementById("mesaj4");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+				var javascriptDivObject=document.getElementById("cevab4v1");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+				var javascriptDivObject=document.getElementById("cevab4v2");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+				var javascriptDivObject=document.getElementById("cevab4v3");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+				var javascriptDivObject=document.getElementById("cevab4v4");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+            }
+		    var sonuc=document.getElementById("yanlis");
+			sonuc.value=Number(sonuc.value)+1;
+			var sonuc=document.getElementById("toplam");
+			sonuc.value=Number(sonuc.value)+1;
+}	
+//  <---------- 3.soru sonu  ---------->
+function soru4(){  //  <---------- 4.soru   ---------->
+		if("hiding" == status) {
+                alert("zaten gizli durumda");
+            } else {
+				var javascriptDivObject=document.getElementById("s4");
+                javascriptDivObject.style.display="none";
+                status = "hiding";
+				
+				var javascriptDivObject=document.getElementById("mesaj5");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+				var javascriptDivObject=document.getElementById("cevab5v1");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+				var javascriptDivObject=document.getElementById("cevab5v2");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+				var javascriptDivObject=document.getElementById("cevab5v3");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+				var javascriptDivObject=document.getElementById("cevab5v4");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+            }
+		    var sonuc=document.getElementById("dogru");
+			sonuc.value=Number(sonuc.value)+1;
+			var sonuc=document.getElementById("toplam");
+			sonuc.value=Number(sonuc.value)+1;
+}
+      function soruv4(){
+	  if("hiding" == status) {
+                alert("zaten gizli durumda");
+            } else {
+				var javascriptDivObject=document.getElementById("s4");
+                javascriptDivObject.style.display="none";
+                status = "hiding";
+
+				
+				
+				var javascriptDivObject=document.getElementById("mesaj5");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+				var javascriptDivObject=document.getElementById("cevab5v1");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+				var javascriptDivObject=document.getElementById("cevab5v2");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+				var javascriptDivObject=document.getElementById("cevab5v3");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+				var javascriptDivObject=document.getElementById("cevab5v4");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+            }
+		    var sonuc=document.getElementById("yanlis");
+			sonuc.value=Number(sonuc.value)+1;
+			var sonuc=document.getElementById("toplam");
+			sonuc.value=Number(sonuc.value)+1;
+}	
+//  <---------- 4.soru sonu  ---------->
+function soru5(){  //  <---------- 5.soru   ---------->
+		if("hiding" == status) {
+                alert("zaten gizli durumda");
+            } else {
+				var javascriptDivObject=document.getElementById("s5");
+                javascriptDivObject.style.display="none";
+                status = "hiding";
+				
+				var javascriptDivObject=document.getElementById("mesaj6");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+				var javascriptDivObject=document.getElementById("cevab6v1");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+				var javascriptDivObject=document.getElementById("cevab6v2");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+				var javascriptDivObject=document.getElementById("cevab6v3");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+				var javascriptDivObject=document.getElementById("cevab6v4");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+            }
+		    var sonuc=document.getElementById("dogru");
+			sonuc.value=Number(sonuc.value)+1;
+			var sonuc=document.getElementById("toplam");
+			sonuc.value=Number(sonuc.value)+1;
+}
+      function soruv5(){
+	  if("hiding" == status) {
+                alert("zaten gizli durumda");
+            } else {
+				var javascriptDivObject=document.getElementById("s5");
+                javascriptDivObject.style.display="none";
+                status = "hiding";
+
+				
+				var javascriptDivObject=document.getElementById("mesaj6");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+				var javascriptDivObject=document.getElementById("cevab6v1");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+				var javascriptDivObject=document.getElementById("cevab6v2");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+				var javascriptDivObject=document.getElementById("cevab6v3");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+				var javascriptDivObject=document.getElementById("cevab6v4");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+            }
+		    var sonuc=document.getElementById("yanlis");
+			sonuc.value=Number(sonuc.value)+1;
+			var sonuc=document.getElementById("toplam");
+			sonuc.value=Number(sonuc.value)+1;
+}	
+//  <---------- 5.soru sonu  ---------->
+function soru6(){  //  <---------- 6.soru   ---------->
+		if("hiding" == status) {
+                alert("zaten gizli durumda");
+            } else {
+				var javascriptDivObject=document.getElementById("s6");
+                javascriptDivObject.style.display="none";
+                status = "hiding";
+
+				
+				var javascriptDivObject=document.getElementById("span");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+
+
+            }
+		    var sonuc=document.getElementById("dogru");
+			sonuc.value=Number(sonuc.value)+1;
+			var sonuc=document.getElementById("toplam");
+			sonuc.value=Number(sonuc.value)+1;
+}
+      function soruv6(){
+	  if("hiding" == status) {
+                alert("zaten gizli durumda");
+            } else {
+				var javascriptDivObject=document.getElementById("s6");
+                javascriptDivObject.style.display="none";
+                status = "hiding";
+
+
+				var javascriptDivObject=document.getElementById("span");
+                javascriptDivObject.style.display="block";
+                status = "showing";
+
+            }
+		    var sonuc=document.getElementById("yanlis");
+			sonuc.value=Number(sonuc.value)+1;
+			var sonuc=document.getElementById("toplam");
+			sonuc.value=Number(sonuc.value)+1;
+}	
+//  <---------- 6.soru VE BİTTİ  ---------->
